98486332b0846d9bf7cae2d352e9e884b9ffa1a2
Melissa <melissainui@gmail.com>
2021-11-25 03:15:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.list import MDList, OneLineIconListItem
 from kivy.properties import StringProperty
 import psycopg2
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 KV = '''
 #:import get_color_from_hex kivy.utils.get_color_from_hex
 
-#:set text_color get_color_from_hex("#4a4939")
-#:set focus_color get_color_from_hex("#e7e4c0")
-#:set bg_color get_color_from_hex("#f7f4e7")
-#:set selected_color get_color_from_hex("#0c6c4d")
+
+ScreenManager:
+    HomePage:
+    FuncionarioPage:
 
 
 <NavigationDrawer>
@@ -68,78 +69,179 @@ KV = '''
                     root.screen_manager.current = "item 3"
 
 
-MDScreen:
+<HomePage>:
+    name: 'home'
+    MDScreen:
+        MDToolbar:
+            id: toolbar
+            pos_hint: {"top": 1}
+            elevation: 10
+            title: "Bakery ABC"
+            left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+            md_bg_color: get_color_from_hex("#854442")
 
-    MDToolbar:
-        id: toolbar
-        pos_hint: {"top": 1}
-        elevation: 10
-        title: "Bakery ABC"
-        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
-        md_bg_color: get_color_from_hex("#854442")
+        MDNavigationLayout:
+            x: toolbar.height
+            ScreenManager:
+                id: screen_manager
+                MDScreen:
+                    name: "item 1"
+                    MDLabel:
+                        text: "Screen 1"
+                        halign: "center"
+                MDScreen:
+                    name: "item 2"
+                    MDLabel:
+                        text: "Screen 2"
+                        halign: "center"
+                MDScreen:
+                    name: "item 3"
+                    MDLabel:
+                        text: "Screen 3"
+                        halign: "center"
+            MDNavigationDrawer:
+                id: nav_drawer
+                NavigationDrawer:
+                    screen_manager: screen_manager
+                    nav_drawer: nav_drawer
+
+        MDBottomNavigation:
+            size_hint_y: .09
+            panel_color: get_color_from_hex("#854442")
+            text_color_active: get_color_from_hex("#fff4e6")
+            
+            MDBottomNavigationItem:
+                name: 'screen 1'
+                text: 'home'
+                icon: 'home-circle'
+                on_enter: root.switchHome()
+
+            MDBottomNavigationItem:
+                name: 'screen 2'
+                text: 'botao 2'
+                icon: 'barcode'
+
+            MDBottomNavigationItem:
+                name: 'screen 3'
+                text: 'Funcionarios'
+                icon: 'account-group'
+                on_enter: root.switchFuncionario()
+                
+ 
+<FuncionarioPage>:
+    name: 'funcionario'
+
+    MDScreen:
+        MDToolbar:
+            id: toolbar
+            pos_hint: {"top": 1}
+            elevation: 10
+            title: "Bakery ABC"
+            left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+            md_bg_color: get_color_from_hex("#854442")
+
+        MDNavigationLayout:
+            x: toolbar.height
+            ScreenManager:
+                id: screen_manager
+                MDScreen:
+                    name: "item 1"
+                    MDLabel:
+                        md_bg_color : get_color_from_hex("#7E6B73")
+                        
+                MDScreen:
+                    name: "item 2"
+                    MDLabel:
+                        text: "Screen 2"
+                        halign: "center"
+                MDScreen:
+                    name: "item 3"
+                    MDLabel:
+                        text: "Screen 3"
+                        halign: "center"
+            MDNavigationDrawer:
+                id: nav_drawer
+                NavigationDrawer:
+                    screen_manager: screen_manager
+                    nav_drawer: nav_drawer
+
+        MDBottomNavigation:
+            size_hint_y: .09
+            panel_color: get_color_from_hex("#854442")
+            text_color_active: get_color_from_hex("#fff4e6")
+            
+            MDBottomNavigationItem:
+                name: 'screen 1'
+                text: 'home'
+                icon: 'home-circle'
+                on_enter: root.switchHome()
+
+            MDBottomNavigationItem:
+                name: 'screen 2'
+                text: 'botao 2'
+                icon: 'barcode'
+
+            MDBottomNavigationItem:
+                name: 'screen 3'
+                text: 'Funcionarios'
+                icon: 'account-group'
+                on_enter: root.switchFuncionario()
+
+        MDFillRoundFlatIconButton :
+            text : 'Cadastrar um empregado'
+            icon : "account-plus"
+            pos_hint : {"center_x":.5,"center_y":.8}
+            text_color : get_color_from_hex("#7E6B73")
+            md_bg_color : get_color_from_hex("#FCE18E")
+            font_size : 20
+            on_press : app.callback()
+            user_font_size: "64sp"
+
+        MDFillRoundFlatIconButton :
+            text : 'Remover um empregado'
+            icon : "account-minus"
+            pos_hint : {"center_x":.5,"center_y":.6}
+            text_color : get_color_from_hex("#7E6B73")
+            md_bg_color : get_color_from_hex("#FCE18E")
+            font_size : 20
+            on_press : app.callback()
+
+        MDFillRoundFlatIconButton :
+            text : 'Alterar dados de um empregado'
+            icon : "account-edit"
+            pos_hint : {"center_x":.5,"center_y":.4}
+            text_color : get_color_from_hex("#7E6B73")
+            md_bg_color : get_color_from_hex("#FCE18E")
+            font_size : 20
+            on_press : app.callback()
 
 
-    MDNavigationLayout:
-        x: toolbar.height
-
-        ScreenManager:
-            id: screen_manager
-
-            MDScreen:
-                name: "item 1"
-
-                MDLabel:
-                    text: "Screen 1"
-                    halign: "center"
-
-            MDScreen:
-                name: "item 2"
-
-                MDLabel:
-                    text: "Screen 2"
-                    halign: "center"
-
-            MDScreen:
-                name: "item 3"
-
-                MDLabel:
-                    text: "Screen 3"
-                    halign: "center"
-
-        MDNavigationDrawer:
-            id: nav_drawer
-
-            NavigationDrawer:
-                screen_manager: screen_manager
-                nav_drawer: nav_drawer
-
-
-    MDBottomNavigation:
-        size_hint_y: .09
-        panel_color: get_color_from_hex("#854442")
-        text_color_active: get_color_from_hex("#fff4e6")
-
-        MDBottomNavigationItem:
-            name: 'screen 1'
-            text: 'home'
-            icon: 'home-circle'
-
-        MDBottomNavigationItem:
-            name: 'screen 2'
-            text: 'botao 2'
-            icon: 'barcode'
-
-        MDBottomNavigationItem:
-            name: 'screen 3'
-            text: 'botao 3'
-            icon: 'account-circle-outline'
 
 '''
+
+
+class HomePage(Screen):
+    def switchHome(self):
+        self.parent.current = 'home'
+    def switchFuncionario(self):
+        self.parent.current = 'funcionario'
+
+class FuncionarioPage(Screen):
+    def switchHome(self):
+        self.parent.current = 'home'
+    def switchFuncionario(self):
+        self.parent.current = 'funcionario'
 
 
 class NavigationDrawer(MDBoxLayout):
     screen_manager = ObjectProperty()
     nav_drawer = ObjectProperty()
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(HomePage(name='home'))
+sm.add_widget(FuncionarioPage(name='funcionario'))
+
 
 class Main(MDApp):
     def build(self):
